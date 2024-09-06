1969c7650a68e98196c8e52746b59463ac363b10
JuliaMelle <54196660+JuliaMelle@users.noreply.github.com>
2024-09-06 22:52:36 +0800

    Dashboard: List of Tags

--- src/pages/layouts/layout.tsx
+++ src/pages/layouts/layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: { children: React.ReactNode }) {
       <Navbar />
       <div className="flex min-h-screen flex-col">
         <main className="flex grow items-center justify-center px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
-          <div className="w-full max-w-4xl">{children}</div>
+          <div className="w-full">{children}</div>
         </main>
         <Footer />
       </div>
--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,5 +1,31 @@
+import Card from '../components/card';
 import Layout from '../layouts/layout';
 
+const cardData = [
+  { id: 1, title: 'Title 1', task: 'Task 1' },
+  { id: 2, title: 'Title 2', task: 'Task 2' },
+  { id: 3, title: 'Title 3', task: 'Task 3' },
+  { id: 4, title: 'Title 4', task: 'Task 4' },
+  { id: 5, title: 'Title 5', task: 'Task 5' },
+  { id: 6, title: 'Title 6', task: 'Task 6' },
+  { id: 7, title: 'Title 7', task: 'Task 7' },
+  { id: 8, title: 'Title 8', task: 'Task 8' },
+  { id: 9, title: 'Title 9', task: 'Task 9' },
+  { id: 10, title: 'Title 10', task: 'Task 10' },
+  { id: 11, title: 'Title 11', task: 'Task 11' },
+  { id: 12, title: 'Title 12', task: 'Task 12' },
+];
+
 export default function Tags() {
-  return <Layout>hello tags</Layout>;
+  return (
+    <Layout>
+      <div className=" m-10  flex  flex-wrap items-center justify-center gap-4 rounded-xl bg-cyan-900 py-10">
+        {cardData.map((card) => (
+          <div key={card.id} className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/5">
+            <Card title={card.title} task={card.task} index={card.id} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
